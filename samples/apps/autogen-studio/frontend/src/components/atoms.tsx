eb7bb72543ba1d83c25bb95b2693eb33dfdf1117
杨茂聪 <yangmaocong@cvte.com>
2024-05-09 11:04:57 +0800

    +

--- samples/apps/autogen-studio/frontend/src/components/atoms.tsx
+++ samples/apps/autogen-studio/frontend/src/components/atoms.tsx
@@ -2115,7 +2115,7 @@ export const AgentRow = ({ message }: { message: any }) => {
       }
       className="m"
     >
-      // modify by ymc
+      {/* modify by ymc */}
       <MarkdownView data={message.message?.content||JSON.stringify(message.message?.function_call||message.message?.tool_calls)} className="text-sm" />
     </GroupView>
   );
