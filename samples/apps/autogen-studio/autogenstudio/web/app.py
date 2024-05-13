7e71fb6b1517f08d88d842b8f5fca88c5f53a8c7
杨茂聪 <yangmaocong@cvte.com>
2024-05-13 21:40:30 +0800

    fix: allow rogins *

--- samples/apps/autogen-studio/autogenstudio/web/app.py
+++ samples/apps/autogen-studio/autogenstudio/web/app.py
@@ -69,10 +69,7 @@ app = FastAPI(lifespan=lifespan)
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://localhost:8000",
-        "http://127.0.0.1:8000",
-        "http://localhost:8001",
-        "http://localhost:8081",
+        "*",        
     ],
     allow_credentials=True,
     allow_methods=["*"],
