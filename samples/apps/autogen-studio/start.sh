1dd7c73edf836f05cdd3a6ce14b79718edf15881
杨茂聪 <yangmaocong@cvte.com>
2024-06-12 15:53:19 +0800

    fix: start.sh support source build

--- samples/apps/autogen-studio/start.sh
+++ samples/apps/autogen-studio/start.sh
@@ -1,1 +1,5 @@
-pip3 install -U gunicorn pyautogen && pip3 install -e . && export AUTOGENSTUDIO_APPDIR=/app/autogenstudio-data && gunicorn -w 5 --timeout 12600 -k uvicorn.workers.UvicornWorker autogenstudio.web.app:app --bind "0.0.0.0:8081"
+pip3 install ./autogen
+pip3 install ./autogen/samples/apps/autogen-studio
+pip3 install -U gunicorn
+export AUTOGENSTUDIO_APPDIR=/app/autogenstudio-data
+gunicorn -w 5 --timeout 12600 -k uvicorn.workers.UvicornWorker autogenstudio.web.app:app --bind "0.0.0.0:8081"
