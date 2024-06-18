2c6c14e48ee16d06f0c6821dce128fa9ad5ce52c
杨茂聪 <yangmaocong@cvte.com>
2024-06-18 15:42:55 +0800

    fix: 去除其它没用的SheEditorObjectSymbol

--- samples/apps/autogen-studio/autogenstudio/plugins/en_exam/ExamGeometryCreateAgent.py
+++ samples/apps/autogen-studio/autogenstudio/plugins/en_exam/ExamGeometryCreateAgent.py
@@ -87,8 +87,8 @@ class SheEditorGeoConeComponent(object):
         result = asdict(self)
         return result    
 
-SheEditorObjectSymbol = Literal["cube", "sphere", "cylinder", "cone", "pyramid", "capsule", "torus", "text3d", "anchorLabel", "square", "circle", "polyline", "polygon", "geoCube", "geoCylinder", "geoCone"]
-SheEditorComponentTypeSymbol = Literal["transform", "standardMaterial", "geoCylinder", "geoCube", "geoCone", "geoCone"]
+SheEditorObjectSymbol = Literal["geoCube", "geoCylinder", "geoCone"]
+SheEditorComponentTypeSymbol = Literal["transform", "standardMaterial", "geoCube", "geoCylinder", "geoCone"]
 
 def createObject(
     objectType: Annotated[SheEditorObjectSymbol, "3d几何体类型"],  
