9cc5285154ab16dd9baeeb0060b88ef22d062349
杨茂聪 <yangmaocong@cvte.com>
2024-06-24 09:53:48 +0800

    wheel包离线方式安装

--- samples/apps/autogen-studio/start.sh
+++ samples/apps/autogen-studio/start.sh
@@ -1,10 +1,3 @@
-cd ./autogen
-pip install --no-index --find-links=packages -r requirements.txt
-
-cd ./samples/apps/autogen-studio
-pip install --no-index --find-links=packages -r requirements.txt
-
-cd ../../../../
-pip3 install -U gunicorn
+pip install --no-index --find-links=wheels -r packages.txt
 
 gunicorn -w 5 --timeout 12600 -k uvicorn.workers.UvicornWorker autogenstudio.web.app:app --bind "0.0.0.0:8081"
