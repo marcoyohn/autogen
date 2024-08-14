ba203670f2c17ec8bebdb3dd51e6545001519374
杨茂聪 <yangmaocong@cvte.com>
2024-08-14 12:43:40 +0800

    fix:  autogenstudio ui --host="0.0.0.0" --port=8081

--- samples/apps/autogen-studio/start.sh
+++ samples/apps/autogen-studio/start.sh
@@ -1,3 +1,4 @@
 pip install --no-index --find-links=wheels -r packages.txt
 
-gunicorn -w 5 --timeout 12600 -k uvicorn.workers.UvicornWorker autogenstudio.web.app:app --bind "0.0.0.0:8081"
+# gunicorn -w 5 --timeout 12600 -k uvicorn.workers.UvicornWorker autogenstudio.web.app:app --bind "0.0.0.0:8081"
+autogenstudio ui --host="0.0.0.0" --port=8081
